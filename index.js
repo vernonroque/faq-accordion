38033302198834a3f803ec419bb2fd4c0f0be39e
vernonroque <vroque88@gmail.com>
2024-01-05 12:29:58 -0500

    Made buttons work

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const main = () => {
-$('.plus-icon').on('click',(event)=>{
-    console.log("I got clicked baus",event.currentTarget);
+$('.answer-icon').on('click',(event)=>{
+    const srcAttr = $(event.currentTarget).attr("src");
+    console.log(srcAttr);
     //closest() traverses up the dom tree to find the closest ancestor
     $(event.currentTarget).closest('.question').find('.answer').toggleClass('active'); 
+    if(srcAttr === './assets/images/icon-plus.svg')
+         $(event.currentTarget).attr("src", "./assets/images/icon-minus.svg");
+    else
+         $(event.currentTarget).attr("src", "./assets/images/icon-plus.svg");
     
-
 })
 
 }
