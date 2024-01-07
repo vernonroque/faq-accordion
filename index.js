5e06f6d3a219c1f33536aa5cdea96d5fe46ff5be
vernonroque <vroque88@gmail.com>
2024-01-07 15:13:56 -0500

    Added more functionality and mobile design

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const main = () => {
 $('.answer-icon').on('click',(event)=>{
+     event.stopPropagation(); // Stop event propagation
     const srcAttr = $(event.currentTarget).attr("src");
     console.log(srcAttr);
     //closest() traverses up the dom tree to find the closest ancestor
@@ -8,7 +9,16 @@ $('.answer-icon').on('click',(event)=>{
          $(event.currentTarget).attr("src", "./assets/images/icon-minus.svg");
     else
          $(event.currentTarget).attr("src", "./assets/images/icon-plus.svg");
-    
+})
+
+$('.question-headliner').on('click',(event)=>{
+     const srcAttr = $(event.currentTarget).find('.answer-icon').attr("src");
+     console.log("The src>>>", srcAttr);
+     $(event.currentTarget).next().toggleClass('active');
+     if(srcAttr === './assets/images/icon-plus.svg')
+         $(event.currentTarget).find('.answer-icon').attr("src", "./assets/images/icon-minus.svg");
+    else
+         $(event.currentTarget).find('.answer-icon').attr("src", "./assets/images/icon-plus.svg");
 })
 
 }
